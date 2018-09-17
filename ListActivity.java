abaf166a375b153715dc7fa00866850e2d7bf486
Milan Bhuva <msbhuva9971@gmail.com>
2018-09-17 13:35:14 +0530

    Create ListActivity.java

---
+++ ListActivity.java
@@ -0,0 +1,7 @@
+// use of list view
+        this.mListView = (ExpandableHeightListView) rootView.findViewById(R.id.listView);
+        this.mListView.setExpanded(true);
+        
+// use of grid view
+        this.mGridView = (ExpandableHeightGridView) rootView.findViewById(R.id.gridView);
+        this.mGridView.setExpanded(true);
